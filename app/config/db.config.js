b0a691e7760525cc397dde9c07a9cc110600bda2
kjenson <kenneth.jenson@gmail.com>
2021-04-13 09:21:44 -0600

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "jenson-mysql.c8qwuofu4md8.us-east-1.rds.amazonaws.com",
+    USER: "kennyjay",
+    PASSWORD: "mysqluser",
+    DB: "RestCustomers"
+  };
+  // module.exports = {
+  //   HOST: "aa1jhxqco4uuy72.c8qwuofu4md8.us-east-1.rds.amazonaws.com",
+  //   USER: "kennyjay",
+  //   PASSWORD: "mysqluser",
+  //   DB: "ebdb"
+  // };
+  
